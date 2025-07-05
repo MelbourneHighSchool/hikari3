3a0e845b6823ef6ff80cf6079f793d9b8280a904
RandomUsernames20847 <abcdefghjiklmnopqrstuvwxyz670@gmail.com>
2025-07-05 19:11:29 +1000

    Added to ObjectDetection2 (wip)
    - Finished ball_proc (see more below)
    - Created goal_proc
    - Readouts for each object are now stored in shm objs
    - ^ Introduced struct packing to store info
    - Added termination signal (though it's unused so far) for graceful exit
    To do:
    - Start function
    - Callback
    - Merge goals, finish goal_proc

--- core/subsystems/ObjectDetection2.py
+++ core/subsystems/ObjectDetection2.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import multiprocessing.shared_memory
 import multiprocessing.synchronize
-import picamera2
+import struct
 import cv2
 import numpy as np
 import time
@@ -9,6 +9,24 @@ import time
 OBJ_BALL = 0
 OBJ_BLUEGOAL = 1
 OBJ_YELLOWGOAL = 2
+
+READOUT_FMT_BALL = "fHHB"
+# float: Ball angle (rad)
+# ushort: Ball distance (mm, 0 if not found)
+# ushort: Ball img x (px)
+# ushort: Ball img y (px)
+# ubyte: Ball img radius (px)
+
+READOUT_FMT_GOAL = "HHHHHHHH" + "HHHHHHHH" + "HHHHHHHH" + "ffff" + "?"
+# ushort (8): Bounding box of chunk 1 (px)
+# ushort (8): Bounding box of chunk 2 (px)
+# ushort (8): Bounding box of combined goal (px)
+# float (2): Start and end angle of chunk 1 (rad)
+# float (2): Start and end angle of chunk 2 (rad)
+# bool: How many goal chunks detected (0-2)
+
+FMT_BALL_SIZE = struct.calcsize(READOUT_FMT_BALL)
+FMT_GOAL_SIZE = struct.calcsize(READOUT_FMT_GOAL)
 
 class ObjectDetection:
     def __init__(self, components, subsystems, is_main = True):
@@ -48,34 +66,37 @@ class ObjectDetection:
         self.center = multiprocessing.Array("i", [500, 400])
         self.resolution = multiprocessing.Array("i", [1000, 800])
 
-        self.readout_ball_angle = multiprocessing.Value("f", 0.0)
-        self.readout_ball_realdist = multiprocessing.Value("H", 0)
+        # Readout data
+        self.readout_ball = multiprocessing.shared_memory.SharedMemory(create=True, size=FMT_BALL_SIZE, name="readout_ball")
+        self.readout_bluegoal = multiprocessing.shared_memory.SharedMemory(create=True, size=FMT_GOAL_SIZE, name="readout_bluegoal")
+        self.readout_yellowgoal = multiprocessing.shared_memory.SharedMemory(create=True, size=FMT_GOAL_SIZE, name="readout_yellowgoal")
 
         self.frame_rgb = np.ndarray(buffer=self.rgb_frames_shm.buf, shape=[*self.camera.resolution, 3], dtype=np.uint8)
         self.frame_hsv = np.ndarray(buffer=self.hsv_frames_shm.buf, shape=[*self.camera.resolution, 3], dtype=np.uint8)
 
         # Events for terminating child processes
-        worker_terminate_event = multiprocessing.Event()
-    
+        self.worker_terminate_event = multiprocessing.Event()
+
     def start(self, frame_hsv: cv2.Mat):
         self.pool = multiprocessing.Pool(processes=1)
-        tasks = [
-            self.pool.apply_async(self.ball_proc, args=(frame_hsv, (self.hsv_time, self.ball_time))),
-        ]
+        self.pool.apply_async(self.ball_proc, args=(
+            (self.center, self.resolution, self.OBJ_SETTINGS),  # Shared arrs
+            (self.hsv_time, self.ball_time),                    # Shared vars
+            self.worker_terminate_event                         # Terminate event
+        ))
         self.pool.close()
         self.pool.join()
     
     @staticmethod
-    def ball_proc(shared_arrs: tuple[3], shared_vars: tuple[4], terminate_event: multiprocessing.synchronize.Event):
+    def ball_proc(shared_arrs: tuple[3], shared_vars: tuple[2], terminate_event: multiprocessing.synchronize.Event):
         # Shared vars
         hsv_time = shared_vars[0]
         ball_time = shared_vars[1]
-        readout_ball_angle = shared_vars[2]
-        readout_ball_realdist = shared_vars[3]
 
         # Shared arrs [Note: must restart process to change]
         center = shared_arrs[0]
         resolution = shared_arrs[1]
+        readout_ball_shm = multiprocessing.shared_memory.SharedMemory(create=False, name="readout_ball")
         obj_settings = shared_arrs[2][7 * OBJ_BALL:7 * OBJ_BALL + 7]
         hsv_frames_shm = multiprocessing.shared_memory.SharedMemory(create=False, name="hsv_stream")
         frame_hsv = np.ndarray(buffer=hsv_frames_shm.buf, shape=[*resolution, 3], dtype=np.uint8)
@@ -83,15 +104,11 @@ class ObjectDetection:
         # Control variables?
         last_hsv_time = float("-inf")
 
-        while True:  # [!] Must provide exit path
-            # Check if program needs to terminate
-            if terminate_event.is_set():
-                return
-            
+        while not terminate_event.is_set():
             # Wait for new frame
-            while hsv_time.value <= last_hsv_time:
+            while hsv_time.value <= last_hsv_time and not terminate_event.is_set():
                 time.sleep(0.01)
-
+            
             last_hsv_time = hsv_time.value
 
             # Object settings [Note: subject to change]
@@ -105,9 +122,9 @@ class ObjectDetection:
             contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             contours = [c for c in contours if cv2.contourArea(c) >= min_contour_size]
             if not contours:
-                # If no contours angle and distance = 0
-                readout_ball_realdist.value = 0
-                readout_ball_angle.value = 0
+                # If no contours, distance = 0 indicates it's not found
+                struct.pack_into(READOUT_FMT_BALL, readout_ball_shm.buf, 0,
+                                 0, 0, 0, 0)
                 ball_time.value = time.monotonic()
                 continue
             
@@ -124,8 +141,72 @@ class ObjectDetection:
             y -= center[0]
 
             # Update information
-            readout_ball_realdist.value, readout_ball_angle.value = ObjectDetection.rect_to_polar((x, y))
+            struct.pack_into(READOUT_FMT_BALL, readout_ball_shm.buf, 0,
+                             )
             ball_time.value = time.monotonic()
+    
+    @staticmethod
+    def goal_proc(obj: int, shared_arrs: tuple[3], shared_vars: tuple[4], terminate_event: multiprocessing.synchronize.Event):
+        # Shared vars
+        hsv_time = shared_vars[0]
+        goal_time = shared_vars[1]
+
+        # Shared arrs
+        center = shared_arrs[0]
+        resolution = shared_arrs[1]
+        hsv_frames_shm = multiprocessing.shared_memory.SharedMemory(create=False, name="hsv_stream")
+        frame_hsv = np.ndarray(buffer=hsv_frames_shm.buf, shape=[*resolution, 3], dtype=np.uint8)
+
+        if obj == OBJ_BLUEGOAL:
+            readout_goal_shm = multiprocessing.shared_memory.SharedMemory(create=False, name="readout_bluegoal")
+            obj_settings = shared_arrs[2][7 * OBJ_BLUEGOAL:7 * OBJ_BLUEGOAL + 7]
+        elif obj == OBJ_YELLOWGOAL:
+            readout_goal_shm = multiprocessing.shared_memory.SharedMemory(create=False, name="readout_yellowgoal")
+            obj_settings = shared_arrs[2][7 * OBJ_YELLOWGOAL:7 * OBJ_YELLOWGOAL + 7]
+        
+        # Control variables?
+        last_hsv_time = hsv_time.value()
+
+        while not terminate_event.is_set():
+            # Wait for new frame
+            while hsv_time.value <= last_hsv_time and not terminate_event.is_set():
+                time.sleep(0.01)
+
+            last_hsv_time = hsv_time.value()
+
+            # Object settings [Note: subject to change]
+            min_contour_size = obj_settings[0]
+            lbound = obj_settings[1:4]
+            ubound = obj_settings[4:7]
+
+            # Create mask
+            mask = cv2.inRange(frame_hsv, lbound, ubound)
+
+            # Find contours
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = [c for c in contours if cv2.contourArea(c) >= min_contour_size]
+            if not contours:
+                # If no contours, set # goals found -> 0
+                readout_goal_shm.buf[-1] = 0
+                goal_time.value = time.monotonic()
+                continue
+
+            ### Waiting on code to merge goals bc i cbb
+            # Find the *best* *rotated* rectangle (copilot help me make this get the biggest rotated rect rather than normal rect) # Copilot?
+            max_area = 0
+            rect = (0, 0, 0, 0)
+            for c in contours:
+                # Get the rotated rectangle
+                box = cv2.minAreaRect(c)
+                area = box[1][0] * box[1][1]
+                if area > max_area:
+                    max_area = area
+                    rect = box
+
+            # Update information
+            struct.pack_into(READOUT_FMT_GOAL, readout_goal_shm.buf, 0,
+                             *ObjectDetection.rect_to_polar(rect), *rect)
+            goal_time.value = time.monotonic()
 
     @staticmethod
     def imgdist_to_realdist(imgdist: float):
