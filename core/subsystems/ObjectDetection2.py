4c2634041fc362de935808bf5ab735b4bb1f171d
RandomUsernames20847 <abcdefghjiklmnopqrstuvwxyz670@gmail.com>
2025-07-06 22:52:13 +1000

    Added destructor, Readout type

--- core/subsystems/ObjectDetection2.py
+++ core/subsystems/ObjectDetection2.py
@@ -5,6 +5,9 @@ import struct
 import cv2
 import numpy as np
 import time
+import threading
+import logging
+from collections import namedtuple
 
 OBJ_BALL = 0
 OBJ_BLUEGOAL = 1
@@ -17,16 +20,36 @@ READOUT_FMT_BALL = "fHHB"
 # ushort: Ball img y (px)
 # ubyte: Ball img radius (px)
 
-READOUT_FMT_GOAL = "HHHHHHHH" + "HHHHHHHH" + "HHHHHHHH" + "ffff" + "?"
+READOUT_FMT_GOAL = "HHHHHHHH" + "HHHHHHHH" + "HHHHHHHH" + "ffff" + "B"
 # ushort (8): Bounding box of chunk 1 (px)
 # ushort (8): Bounding box of chunk 2 (px)
 # ushort (8): Bounding box of combined goal (px)
 # float (2): Start and end angle of chunk 1 (rad)
 # float (2): Start and end angle of chunk 2 (rad)
-# bool: How many goal chunks detected (0-2)
+# byte: How many goal chunks detected (0-2)
 
 FMT_BALL_SIZE = struct.calcsize(READOUT_FMT_BALL)
 FMT_GOAL_SIZE = struct.calcsize(READOUT_FMT_GOAL)
+
+Readout = namedtuple("ObjectDetectionReadout", [
+    "ball_angle",          # float: Ball angle (rad)
+    "ball_distance",       # ushort: Ball distance (mm, 0 if not found)
+    "ball_x",              # ushort: Ball img x (px)
+    "ball_y",              # ushort: Ball img y (px)
+    "ball_radius",         # ubyte: Ball img radius (px)
+
+    "bluegoal_bbox",       # ushort (8): Bounding box of goal (full) (px)
+    "bluegoal_bbox1",      # ushort (8): Bounding box of chunk 1 (px)
+    "bluegoal_bbox2",      # ushort (8): Bounding box of chunk 2 (px)
+    "bluegoal_detected",   # ubyte: How many goal chunks detected (0-2)
+    "bluegoal_angle",      # float (2): Start and end angle of chunk 1 (rad)
+
+    "yellowgoal_bbox",     # ushort (8): Bounding box of full goal (px)
+    "yellowgoal_bbox1",    # ushort (8): Bounding box of chunk 1 (px)
+    "yellowgoal_bbox2",    # ushort (8): Bounding box of chunk 2 (px)
+    "yellowgoal_detected", # ubyte: How many goal chunks detected (0-2)
+    "yellowgoal_angle",    # float (2): Start and end angle of chunk 2 (rad)
+])
 
 class ObjectDetection:
     def __init__(self, components, subsystems, is_main = True):
@@ -74,12 +97,30 @@ class ObjectDetection:
         self.frame_rgb = np.ndarray(buffer=self.rgb_frames_shm.buf, shape=[*self.camera.resolution, 3], dtype=np.uint8)
         self.frame_hsv = np.ndarray(buffer=self.hsv_frames_shm.buf, shape=[*self.camera.resolution, 3], dtype=np.uint8)
 
-        # Events for terminating child processes
+        # Worker
+        self.worker_thread = None
         self.worker_terminate_event = multiprocessing.Event()
+        self.worker_termination_timeout = 3.0
+        self.pool = None
+    
+    def get_readout(self):
+        readout_ball = struct.unpack(READOUT_FMT_BALL, self.readout_ball.buf)
+        readout_bluegoal = struct.unpack(READOUT_FMT_GOAL, self.readout_bluegoal.buf)
+        readout_yellowgoal = struct.unpack(READOUT_FMT_GOAL, self.readout_yellowgoal.buf)
 
-    def start(self, frame_hsv: cv2.Mat):
+        return Readout(*readout_ball, *readout_bluegoal, *readout_yellowgoal)
+    
+    def start(self):
+        if self.worker_thread is not None:
+            logging.warning("ObjectDetection.start() called when already started")
+            return True
+        self.worker_thread = threading.Thread(target=self._worker, daemon=True)
+        self.worker_thread.start()
+        return False
+
+    def _worker(self):
         self.pool = multiprocessing.Pool(processes=1)
-        self.pool.apply_async(self.ball_proc, args=(
+        self.pool.apply_async(self._ball_proc, args=(
             (self.center, self.resolution, self.OBJ_SETTINGS),  # Shared arrs
             (self.hsv_time, self.ball_time),                    # Shared vars
             self.worker_terminate_event                         # Terminate event
@@ -87,8 +128,33 @@ class ObjectDetection:
         self.pool.close()
         self.pool.join()
     
+    def _stop_async(self, is_destructor=False):
+        if is_destructor:
+            if self.pool is not None:
+                self.pool.terminate()
+                self.pool = None
+            if self.worker_thread is not None:
+                self.worker_thread = None
+            return
+        
+        if self.worker_thread is None:
+            logging.warning("ObjectDetection worker thread is None, cannot stop")
+            return
+        
+        self.worker_thread.join(timeout=self.worker_termination_timeout)
+        
+        if self.worker_thread.is_alive():
+            self.pool.terminate()
+            logging.warning(f"ObjectDetection worker did not terminate within timeout of {self.worker_termination_timeout}s, forcefully terminated")
+        
+        self.worker_thread = None
+        self.pool = None
+
+    def stop(self):
+        stopt = threading.Thread(target=self._stop_async, daemon=True).start()
+
     @staticmethod
-    def ball_proc(shared_arrs: tuple[3], shared_vars: tuple[2], terminate_event: multiprocessing.synchronize.Event):
+    def _ball_proc(shared_arrs: tuple[3], shared_vars: tuple[2], terminate_event: multiprocessing.synchronize.Event):
         # Shared vars
         hsv_time = shared_vars[0]
         ball_time = shared_vars[1]
@@ -105,9 +171,12 @@ class ObjectDetection:
         last_hsv_time = float("-inf")
 
         while not terminate_event.is_set():
-            # Wait for new frame
+            # Wait for new frame, check for termination signal
             while hsv_time.value <= last_hsv_time and not terminate_event.is_set():
                 time.sleep(0.01)
+            
+            if terminate_event.is_set():
+                return
             
             last_hsv_time = hsv_time.value
 
@@ -146,7 +215,7 @@ class ObjectDetection:
             ball_time.value = time.monotonic()
     
     @staticmethod
-    def goal_proc(obj: int, shared_arrs: tuple[3], shared_vars: tuple[4], terminate_event: multiprocessing.synchronize.Event):
+    def _goal_proc(obj: int, shared_arrs: tuple[3], shared_vars: tuple[4], terminate_event: multiprocessing.synchronize.Event):
         # Shared vars
         hsv_time = shared_vars[0]
         goal_time = shared_vars[1]
@@ -168,9 +237,12 @@ class ObjectDetection:
         last_hsv_time = hsv_time.value()
 
         while not terminate_event.is_set():
-            # Wait for new frame
+            # Wait for new frame and check for termination signal
             while hsv_time.value <= last_hsv_time and not terminate_event.is_set():
                 time.sleep(0.01)
+            
+            if terminate_event.is_set():
+                return
 
             last_hsv_time = hsv_time.value()
 
@@ -229,3 +301,26 @@ class ObjectDetection:
         x = r * np.cos(theta)
         y = r * np.sin(theta)
         return (x, y)
+    
+    def __del__(self):
+        # God I dearly hope this works and doesn't only trigger after multiprocessing is uninitialised
+        # Stop child processes, stop worker thread
+        try:
+            self._stop_async()
+        except:
+            logging.warning("ObjectDetection._stop_async() failed")
+        
+        for shm in [
+            self.rgb_frames_shm,
+            self.hsv_frames_shm,
+            self.readout_ball,
+            self.readout_bluegoal,
+            self.readout_yellowgoal
+        ]:
+            try:
+                shm.close()
+                shm.unlink()
+            except FileNotFoundError:
+                logging.warning(f"Shared memory {shm.name} already unlinked")
+            except:
+                logging.warning(f"Shared memory {shm.name} could not be destroyed")
